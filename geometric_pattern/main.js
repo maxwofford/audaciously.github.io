b16efdcf32ea8d6739c448530212e47de5e0ac74
Abigail Clarke <sleepybun98@yahoo.com>
2016-11-01 00:17:02 +0000

    Add colors to geometric_pattern.

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -2,6 +2,13 @@ var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
 
+var NUM_CIRCLES = 12;
+var circleDiameter;
+var circleRadius;
+var rVal;
+var gVal;
+var bVal;
+
 function setup() {
     createCanvas(480, 600);
     circleDiameter = width/NUM_CIRCLES;
@@ -9,7 +16,11 @@ function setup() {
 }
 
 function draw() {
-    var isShifted = false;
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
+    
+     var isShifted = false;
     
     var y = height;
     while (y >= 0) {
@@ -23,11 +34,17 @@ function draw() {
         }
         
         while (x <= width) {
+            fill(color(rVal,gVal,bVal));
+            stroke(color(rVal,gVal,bVal));
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
     
         y = y - circleRadius;
         isShifted = !isShifted;
+        
+        rVal = rVal - 2;
+        gVal = gVal + 7;
+        bVal = bVal + 3;
     }
 }
